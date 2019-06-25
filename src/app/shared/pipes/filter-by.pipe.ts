da7310928d4fb35aaecf15b27a7d043b3c1540a8
MacGregor Thompson <mhthompson86@gmail.com>
2019-06-25 09:23:08 -0400

    forgot to indclude pipes

---
+++ src/app/shared/pipes/filter-by.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filterBy'
+})
+export class FilterByPipe implements PipeTransform {
+
+  transform<T>(values: T[], prop: string, args: string[]): T[] {
+    console.log('wtf?');
+    if (!values || !prop || !args.length) return values;
+    return values.filter(value => args.indexOf(value[prop]) !== -1);
+  }
+
+}
