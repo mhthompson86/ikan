5c23cdf4c66fe738307ab3a1b6645e99c3ceb36b
MacGregor Thompson <mhthompson86@gmail.com>
2020-04-27 19:38:09 -0400

    - did this work? -2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +11,7 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { BoardModule } from './board/board.module';
 import { BacklogModule } from './backlog/backlog.module';
-import { APP_BASE_HREF } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @NgModule({
   declarations: [
@@ -32,7 +32,7 @@ import { APP_BASE_HREF } from '@angular/common';
     // AppRouting module must be declared last in order to prevent the ** from catching
     AppRoutingModule,
   ],
-  providers: [{provide: APP_BASE_HREF, useValue: '/dashboard/'}],
+  providers: [{provide: APP_BASE_HREF, useValue: environment.baseUrl}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
